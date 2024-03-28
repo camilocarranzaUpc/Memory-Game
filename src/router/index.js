76226046bfaeffd259bb0f102b8c352391113b19
camilo carranza <camilo.carranza@estudiantat.upc.edu>
2024-03-28 14:47:10 +0100

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import GameView from '../views/GameView.vue'
+import ScoreView from '../views/ScoreView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: GameView
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: ScoreView
+    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
+})
+
+export default router
